ed09725e005f1ce96b8fe8a9384820e3440a44c1
Md Tanweer Alam <tanweeralam5055@gmail.com>
2026-04-14 10:35:56 +0530

    Create loader.js

---
+++ components/loader.js
@@ -0,0 +1,7 @@
+export function renderLoader() {
+    return `
+        <div style="display: flex; justify-content: center; align-items: center; height: 100px;">
+            <i class="fa fa-spinner fa-spin fa-2x" style="color: #6C63FF;"></i>
+        </div>
+    `;
+}
